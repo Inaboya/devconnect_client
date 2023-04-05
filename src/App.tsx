4e67a5627ba3a4f68fe0db9482c48cf9893bd532
Inaboya <momohdavid21@gmail.com>
2023-04-05 17:18:31 +0100

    build landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './page/LandingPage';
+import './App.css';
 
 const App: React.FC = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          {/* <Route path="/register" element={<RegisterPage />} /> */}
+          {/* <Route path="/login" element={<LoginPage />} /> */}
+          {/* <Route path="/profiles" element={<ProfilesPage />} /> */}
+          {/* <Route path="/profiles/:id" element={<ProfilePage />} /> */}
+          {/* <Route
+            path="dashboard"
+            element={<PrivateRoute component={Dashboard} />}
+          /> */}
+          {/* <Route
+            path="create-profile"
+            element={<PrivateRoute component={ProfileForm} />}
+          /> */}
+          {/* <Route
+            path="edit-profile"
+            element={<PrivateRoute component={ProfileForm} />}
+          /> */}
+          {/* <Route
+            path="add-experience"
+            element={<PrivateRoute component={AddExperience} />}
+          /> */}
+          {/* <Route
+            path="add-education"
+            element={<PrivateRoute component={AddEducation} />}
+          /> */}
+          {/* <Route path="posts" element={<PrivateRoute component={Posts} />} /> */}
+          {/* <Route path="posts/:id" element={<PrivateRoute component={Post} />} /> */}
+          {/* <Route path="/*" element={<NotFound />} /> */}
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
