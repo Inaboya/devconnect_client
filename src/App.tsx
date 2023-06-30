9a327a3328c02a380c29fec03e40e6892c64f0d0
Inaboya <momohdavid21@gmail.com>
2023-06-30 18:45:18 +0100

    update functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "./redux/store";
+import { setAuthToken } from "./utils/setAuthToken";
+import { loadUser } from "./redux/actions/auth";
+import { LOGOUT } from "./utils/typings";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const dispatch = useDispatch<AppDispatch>();
 
-export default App
+  useEffect(() => {
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+
+    dispatch(loadUser());
+
+    window.addEventListener("storage", () => {
+      if (!localStorage.token) dispatch({ type: LOGOUT });
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return <div>App</div>;
+};
+
+export default App;
