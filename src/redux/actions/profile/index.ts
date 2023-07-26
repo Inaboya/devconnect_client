5147eab37c602acf94b6cf58b1dfd8adc15990fe
Inaboya <momohdavid21@gmail.com>
2023-07-26 22:39:45 +0100

    create actions for profile

--- src/redux/actions/profile/index.ts
+++ src/redux/actions/profile/index.ts
@@ -3,10 +3,13 @@
 import { Dispatch } from "redux";
 import useAxios from "../../../utils/useAxios";
 import {
+  ACCOUNT_DELETE_FAILURE,
   GET_PROFILES_FAILURE,
   GET_PROFILES_SUCCESS,
   GET_PROFILE_FAILURE,
   GET_PROFILE_SUCCESS,
+  UPDATE_PROFILE_FAILURE,
+  UPDATE_PROFILE_SUCCESS,
 } from "../actionTypes";
 
 export const getCurrentUserProfile = () => async (dispatch: Dispatch) => {
@@ -67,6 +70,113 @@ export const getProfilesById = (id: string) => async (dispatch: Dispatch) => {
   }
 };
 
-export const createProfile =
-  (formData: any, edit = false) =>
-  async (dispatch: Dispatch) => {};
+export const createProfile = (formData: any) => async (dispatch: Dispatch) => {
+  try {
+    const res = await useAxios.post("/profile", formData);
+
+    dispatch({
+      type: GET_PROFILE_SUCCESS,
+      payload: res.data,
+    });
+
+    return res.data;
+  } catch (error: any) {
+    dispatch({
+      type: GET_PROFILE_FAILURE,
+      payload: error.response.data.errors,
+    });
+  }
+};
+
+export const addExperience = (formData: any) => async (dispatch: Dispatch) => {
+  try {
+    const res = await useAxios.post("/profile/experience", formData);
+
+    dispatch({
+      type: GET_PROFILES_SUCCESS,
+      payload: res.data,
+    });
+
+    return res.data;
+  } catch (error: any) {
+    dispatch({
+      type: UPDATE_PROFILE_FAILURE,
+      payload: error.response.data.errors,
+    });
+
+    return error.response.data.errors;
+  }
+};
+
+export const addEducation = (formData: any) => async (dispatch: Dispatch) => {
+  try {
+    const res = await useAxios.post("/profile/education", formData);
+
+    dispatch({
+      type: UPDATE_PROFILE_SUCCESS,
+      payload: res.data,
+    });
+
+    return res.data;
+  } catch (error: any) {
+    dispatch({
+      type: UPDATE_PROFILE_FAILURE,
+      payload: error.response.data.errors,
+    });
+
+    return error.response.data.errors;
+  }
+};
+
+export const deleteExperience = (id: string) => async (dispatch: Dispatch) => {
+  try {
+    const res = await useAxios.delete(`/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE_SUCCESS,
+      payload: res.data,
+    });
+
+    return res.data;
+  } catch (error: any) {
+    dispatch({
+      type: UPDATE_PROFILE_FAILURE,
+      payload: error.response.data.errors,
+    });
+
+    return error.response.data.errors;
+  }
+};
+
+export const deleteEducation = (id: string) => async (dispatch: Dispatch) => {
+  try {
+    const res = await useAxios.delete(`/profile/education/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE_SUCCESS,
+      payload: res.data,
+    });
+
+    return res.data;
+  } catch (error: any) {
+    dispatch({
+      type: UPDATE_PROFILE_FAILURE,
+      payload: error.response.data.errors,
+    });
+
+    return error.response.data.errors;
+  }
+};
+
+// export const deleteAccount = () => async (dispatch: Dispatch) => {
+//   try {
+//     const res = await useAxios.delete("")
+//   } catch (error: any) {
+//     dispatch({
+//       type: ACCOUNT_DELETE_FAILURE,
+//       payload: error.response.data.errors,
+//     })
+
+//     return error.response.data.errors
+//   }
+// }
