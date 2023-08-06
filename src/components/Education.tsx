8a536b8e8bd1f4fccc17e56b40676747d8b98f31
Inaboya <momohdavid21@gmail.com>
2023-08-06 22:33:17 +0100

    build education page and functionality

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,14 +1,56 @@
-import React from 'react'
-import { EducationFormData } from '../utils/typings';
+import React, { Fragment } from "react";
+import { EducationFormData, formatDate } from "../utils/typings";
+import { deleteEducation } from "../redux/actions/profile";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
 interface EducationProps {
-  education: EducationFormData[]
+  education: EducationFormData[];
+  deleteEducation: (id: string) => void;
 }
 
-const Education: React.FC<EducationProps> = () => {
+const Education: React.FC<EducationProps> = ({
+  education,
+  deleteEducation,
+}) => {
+  const educations = education.map((edu: any) => (
+    <tr key={edu._id}>
+      <td>{edu.school}</td>
+      <td className="hide-sm">{edu.degree}</td>
+      <td>
+        {formatDate(edu.from)} - {edu.to ? formatDate(edu.to) : "Now"}
+      </td>
+      <td>
+        <button
+          onClick={() => deleteEducation(edu._id)}
+          className="btn btn-danger"
+        >
+          Delete
+        </button>
+      </td>
+    </tr>
+  ));
   return (
-    <div>Education</div>
-  )
-}
+    <Fragment>
+      <h2 className="my-2">Education Credentials</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>School</th>
+            <th className="hide-sm">Degree</th>
+            <th className="hide-sm">Years</th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{educations}</tbody>
+      </table>
+    </Fragment>
+  );
+};
 
-export default Education
+Education.propTypes = {
+  education: PropTypes.array.isRequired,
+  deleteEducation: PropTypes.func.isRequired,
+};
+
+export default connect(null, { deleteEducation })(Education);
