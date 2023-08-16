a141068b16641d76020d964b5ddc4e5e6455b915
Inaboya <momohdavid21@gmail.com>
2023-08-16 16:56:23 +0100

    build edit experience page

---
+++ src/page/AddExperience.tsx
@@ -0,0 +1,122 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { addExperience } from "../redux/actions/profile";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+toast.configure();
+
+const AddExperience: React.FC = () => {
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = React.useState({
+    company: "",
+    title: "",
+    location: "",
+    from: "",
+    to: "",
+    current: false as boolean | string,
+    description: "",
+  });
+
+  const { company, title, location, from, to, current, description } = formData;
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    const onSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        addExperience(formData);
+
+
+        navigate('/dashboard');
+    }
+
+  return ( <section className="container">
+  <h1 className="large text-primary">Add An Experience</h1>
+  <p className="lead">
+    <i className="fas fa-code-branch" /> Add any developer/programming
+    positions that you have had in the past
+  </p>
+  <small>* = required field</small>
+  <form
+    className="form"
+    onSubmit={onSubmit}
+  >
+    <div className="form-group">
+      <input
+        type="text"
+        placeholder="* Job Title"
+        name="title"
+        value={title}
+        onChange={onChange}
+        required
+      />
+    </div>
+    <div className="form-group">
+      <input
+        type="text"
+        placeholder="* Company"
+        name="company"
+        value={company}
+        onChange={onChange}
+        required
+      />
+    </div>
+    <div className="form-group">
+      <input
+        type="text"
+        placeholder="Location"
+        name="location"
+        value={location}
+        onChange={onChange}
+      />
+    </div>
+    <div className="form-group">
+      <h4>From Date</h4>
+      <input type="date" name="from" value={from} onChange={onChange} />
+    </div>
+    <div className="form-group">
+      <p>
+        <input
+          type="checkbox"
+          name="current"
+          checked={current === true}
+          value={current === true ? "true" : "false"}
+          onChange={onChange}
+        />{' '}
+        Current Job
+      </p>
+    </div>
+    <div className="form-group">
+      <h4>To Date</h4>
+      <input
+        type="date"
+        name="to"
+        value={to}
+        onChange={onChange}
+        disabled={current as boolean}
+      />
+    </div>
+    <div className="form-group">
+      <textarea
+        name="description"
+        cols={30}
+        rows={5}
+        placeholder="Job Description"
+        value={description}
+        onChange={onChange}
+      />
+    </div>
+    <input type="submit" className="btn btn-primary my-1" />
+    <Link className="btn btn-light my-1" to="/dashboard">
+      Go Back
+    </Link>
+  </form>
+</section>
+  );
+};
+
+export default AddExperience;
